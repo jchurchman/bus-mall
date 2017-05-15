73aed988def0f374d7fa35a0ef70dfcf0bd27424
Joe Churchman <churchman.joe@gmail.com>
2017-05-15 14:43:30 -0700

    Declare global variables, object constructor

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 'use strict'
+
+var lastGroup = [];
+var thisGroup = [];
+var busMallItems = [];
+
+function catalogItem ( itemId, itemName, itemFilepath) {
+    this.itemId = itemId;
+    this.itemName = itemName;
+    this.itemFilepath = itemFilepath;
+    this.noTimesShown = 0;
+    this.noTimesClicked = 0;
+    busMallItems.push(this);
+}
