3c133f17f5e87bb5ba13038a0113192756029b74
Joe Churchman <churchman.joe@gmail.com>
2017-05-15 18:25:10 -0700

    Populate 3 different images on page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var lastGroup = [];
 var thisGroup = [];
@@ -13,46 +13,102 @@ function catalogItem ( itemId, itemName, itemFilepath) {
     busMallItems.push(this);
 }
 
-catalogItem.prototype.randomIndex = function (arr) {
-    Math.floor(Math.random() * arr.length);
-};
+function instantiateOptions() {
+    var bag = new catalogItem( 'bag', 'Bag', './imgs/newbag.jpg' );
+    var banana = new catalogItem ( 'banana', 'Banana', './imgs/newbanana.jpg' );
+    var bathroom = new catalogItem ( 'bathroom', 'Bathroom', './imgs/newbathroom.jpg' );
+    var boots = new catalogItem ( 'boots', 'Boots', '../imgs/newboots.jpg' );
+    var breakfast = new catalogItem ( 'breakfast', 'Breakfast', './imgs/newbreakfast.jpg' );
+    var bubblegum = new catalogItem ( 'bubblegum', 'Bubblegum', './imgs/newbubblegum.jpg' );
+    var chair = new catalogItem ( 'chair', 'Chair', './imgs/newchair.jpg' );
+    var cthulhu = new catalogItem ( 'cthulhu', 'Cthulhu', './imgs/newcthulhu.jpg' );
+    var dogduck = new catalogItem ( 'dogduck', 'Dog-Duck', './imgs/newdog-duck.jpg' );
+    var pen = new catalogItem ( 'pen', 'Pen', './imgs/newpen.jpg' );
+    var petsweep = new catalogItem ( 'petsweep', 'Pet Sweep', './imgs/newpet-sweep.jpg' );
+    var scissors = new catalogItem ( 'scissors', 'Scissors', './imgs/newscissors.jpg' );
+    var shark = new catalogItem ( 'shark', 'Shark', './imgs/newshark.jpg' );
+    var sweep = new catalogItem ( 'sweep', 'Sweep', './imgs/newsweep.png' );
+    var tauntaun = new catalogItem ( 'tauntaun', 'Tauntaun', './imgs/newtauntaun.jpg' );
+    var unicorn = new catalogItem ( 'unicorn', 'Unicorn', './imgs/newunicorn.jpg' );
+    var usb = new catalogItem ( 'usb', 'USB', './imgs/newusb.gif' );
+    var watercan = new catalogItem ( 'watercan', 'Water Can', './imgs/newwater-can.jpg' );
+    var wineglass = new catalogItem ( 'wineglass', 'Wine Glass', './imgs/newwine-glass.jpg' );
+}
 
-catalogItem.prototype.getIndecies = function (arr) {
-    var selectedIndecies = [];
-    while ( selectedIndecies.length < 3 ) {
-        var option = this.randomIndex(arr);
+instantiateOptions();
 
-        if ( selectedIndecies.indexOf( option ) === -1 ) {
-            selectedIndecies.push( option );
+function doAllTheWork () {
+    var randomIndecies = [];
+    while ( randomIndecies.length < 3 ) {
+        var getRandomIndecies = (Math.floor(Math.random() * busMallItems.length)); /* Generates random index number within array length of busMallItems */
+        if ( randomIndecies.indexOf( getRandomIndecies ) === -1 && randomIndecies.indexOf ( lastGroup ) === -1 ) { /* Compares randomly generated index number to previously generated numbers or previously shown numbers */
+            randomIndecies.push(getRandomIndecies); /* If no match from line 44, pushes randomly generated index into randomIndecies array */
         }
     }
-    return selectedIndecies;
-};
-
-var button = document.getElementById('button');
-button.addEventListener('click', addSelection );
-
-function addSelection () {
-
-};
-
-function trackLastOptions () {
-
-}
-
-function populateNewOptions () {
-    var imageLoc = document.getElementsByClassName( 'optionImage' );
-    var radioLoc = document.getElementsByClassName( 'optionRadio' );
-
-}
-
-function selectNewOptions () {
-
+    for (var i = 0; i < 3; i++) {
+        var imageLoc = document.getElementsByClassName('optionImage')[i];  /*grabs an image tag with the class */
+        console.log('imageLoc is ' + imageLoc);
+        var wordLoc = document.getElementsByClassName('optionName')[i];  /*grabs a fig caption tag by class */
+        console.log('wordLoc is ' + wordLoc);
+        var imgSrc = busMallItems[randomIndecies[i]].itemFilepath; /* grabs an image filepath referenced by index */
+        console.log('imgSrc is ' + imgSrc);
+        var nameLoc = busMallItems[randomIndecies[i]].itemName; /* grabs an item name referenced by index */
+        console.log('nameLoc is ' + nameLoc);
+        imageLoc.src=imgSrc; /*appends an image filepath to the image src */
+        wordLoc.textContent = nameLoc; /* adds the image name to the fig caption */
+        busMallItems[randomIndecies[i]].noTimesShown = busMallItems[randomIndecies[i]].noTimesShown + 1; /* adds one to the times shown property of the item manipulated by this loop */
+    }
+    
 }
 
 
 
 
+thisGroup.forEach(function () {
+    console.log( 'lastGroup was ' + lastGroup );
+    lastGroup.shift();
+    console.log( 'lastGroup is ' + lastGroup );
+    lastGroup.push();
+    console.log( 'lastGrup is now ' + lastGroup );
+});
+
+
+
+// catalogItem.prototype.displayOptions = function () {
+//     var randomOptions = this.getIndecies( busMallItems );
+//     var index1 = randomOptions[0];
+//     var index2 = randomOptions[1];
+//     var index3 = randomOptions[2];
+
+//     var option1 = busMallItems[index1];
+//     var option2 = busMallItems[index2];
+//     var option3 = busMallItems[index3];
+
+//     this.option1.innerText = option1.itemName;
+//     this.option2.innerText = option2.itemName;
+//     this.option3.innerText = option3.itemName;
+// }
+
+// var button = document.getElementById('button');
+// button.addEventListener('click', addSelection );
+
+// function addSelection () {
+
+// };
+
+// function trackLastOptions () {
+
+// }
+
+// function populateNewOptions () {
+//     var imageLoc = document.getElementsByClassName( 'optionImage' );
+//     var radioLoc = document.getElementsByClassName( 'optionRadio' );
+
+// }
+
+// function selectNewOptions () {
+
+// }
 
 
 
@@ -76,24 +132,6 @@ function selectNewOptions () {
 
 
 
-function () {
-    var bag = new catalogItem( 'bag', 'Bag', '../imgs/newbag.jpg' );
-    var banana = new catalogItem ( 'banana', 'Banana', '../imgs/newbanana.jpg' );
-    var bathroom = new catalogItem ( 'bathroom', 'Bathroom', '../imgs/newbathroom.jpg' );
-    var boots = new catalogItem ( 'boots', 'Boots', '../imgs/newboots.jpg' );
-    var breakfast = new catalogItem ( 'breakfast', 'Breakfast', '../imgs/newbreakfast.jpg' );
-    var bubblegum = new catalogItem ( 'bubblegum', 'Bubblegum', '../imgs/newbubblegum.jpg' );
-    var chair = new catalogItem ( 'chair', 'Chair', '../imgs/newchair.jpg' );
-    var cthulhu = new catalogItem ( 'cthulhu', 'Cthulhu', '../imgs/newcthulhu.jpg' );
-    var dogduck = new catalogItem ( 'dogduck', 'Dog-Duck', '../imgs/newdog-duck.jpg' );
-    var pen = new catalogItem ( 'pen', 'Pen', '../imgs/newpen.jpg' );
-    var petsweep = new catalogItem ( 'petsweep', 'Pet Sweep', '../imgs/newpet-sweep.jpg' );
-    var scissors = new catalogItem ( 'scissors', 'Scissors', '../imgs/newscissors.jpg' );
-    var shark = new catalogItem ( 'shark', 'Shark', '../imgs/newshark.jpg' );
-    var sweep = new catalogItem ( 'sweep', 'Sweep', '../imgs/newsweep.png' );
-    var tauntaun = new catalogItem ( 'tauntaun', 'Tauntaun', '../imgs/newtauntaun.jpg' );
-    var unicorn = new catalogItem ( 'unicorn', 'Unicorn', '../imgs/newunicorn.jpg' );
-    var usb = new catalogItem ( 'usb', 'USB', '../imgs/newusb.gif' );
-    var watercan = new catalogItem ( 'watercan', 'Water Can', '../imgs/newwater-can.jpg' );
-    var wineglass = new catalogItem ( 'wineglass', 'Wine Glass', '../imgs/newwine-glass.jpg' );
-};
+
+
+
