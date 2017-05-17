2b2f7ef9370ff86839cdc7da8badaa1d528f93d8
Joe Churchman <churchman.joe@gmail.com>
2017-05-17 14:50:02 -0700

    Complete MVP of locally storing data

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var busMallItems = [];
+var clicks = JSON.parse(localStorage.getItem('storedClicks')) || [];
 
-function catalogItem ( itemId, itemName, itemFilepath) {
+function catalogItem(itemId, itemName, itemFilepath) {
     this.itemId = itemId;
     this.itemName = itemName;
     this.itemFilepath = itemFilepath;
@@ -12,25 +13,25 @@ function catalogItem ( itemId, itemName, itemFilepath) {
 }
 
 function instantiateOptions() {
-    var bag = new catalogItem( 'bag', 'Bag', './imgs/newbag.jpg' );
-    var banana = new catalogItem ( 'banana', 'Banana', './imgs/newbanana.jpg' );
-    var bathroom = new catalogItem ( 'bathroom', 'Bathroom', './imgs/newbathroom.jpg' );
-    var boots = new catalogItem ( 'boots', 'Boots', './imgs/newboots.jpg' );
-    var breakfast = new catalogItem ( 'breakfast', 'Breakfast', './imgs/newbreakfast.jpg' );
-    var bubblegum = new catalogItem ( 'bubblegum', 'Bubblegum', './imgs/newbubblegum.jpg' );
-    var chair = new catalogItem ( 'chair', 'Chair', './imgs/newchair.jpg' );
-    var cthulhu = new catalogItem ( 'cthulhu', 'Cthulhu', './imgs/newcthulhu.jpg' );
-    var dogduck = new catalogItem ( 'dogduck', 'Dog-Duck', './imgs/newdog-duck.jpg' );
-    var pen = new catalogItem ( 'pen', 'Pen', './imgs/newpen.jpg' );
-    var petsweep = new catalogItem ( 'petsweep', 'Pet Sweep', './imgs/newpet-sweep.jpg' );
-    var scissors = new catalogItem ( 'scissors', 'Scissors', './imgs/newscissors.jpg' );
-    var shark = new catalogItem ( 'shark', 'Shark', './imgs/newshark.jpg' );
-    var sweep = new catalogItem ( 'sweep', 'Sweep', './imgs/newsweep.png' );
-    var tauntaun = new catalogItem ( 'tauntaun', 'Tauntaun', './imgs/newtauntaun.jpg' );
-    var unicorn = new catalogItem ( 'unicorn', 'Unicorn', './imgs/newunicorn.jpg' );
-    var usb = new catalogItem ( 'usb', 'USB', './imgs/newusb.gif' );
-    var watercan = new catalogItem ( 'watercan', 'Water Can', './imgs/newwater-can.jpg' );
-    var wineglass = new catalogItem ( 'wineglass', 'Wine Glass', './imgs/newwine-glass.jpg' );
+    var bag = new catalogItem('bag', 'Bag', './imgs/newbag.jpg');
+    var banana = new catalogItem('banana', 'Banana', './imgs/newbanana.jpg');
+    var bathroom = new catalogItem('bathroom', 'Bathroom', './imgs/newbathroom.jpg');
+    var boots = new catalogItem('boots', 'Boots', './imgs/newboots.jpg');
+    var breakfast = new catalogItem('breakfast', 'Breakfast', './imgs/newbreakfast.jpg');
+    var bubblegum = new catalogItem('bubblegum', 'Bubblegum', './imgs/newbubblegum.jpg');
+    var chair = new catalogItem('chair', 'Chair', './imgs/newchair.jpg');
+    var cthulhu = new catalogItem('cthulhu', 'Cthulhu', './imgs/newcthulhu.jpg');
+    var dogduck = new catalogItem('dogduck', 'Dog-Duck', './imgs/newdog-duck.jpg');
+    var pen = new catalogItem('pen', 'Pen', './imgs/newpen.jpg');
+    var petsweep = new catalogItem('petsweep', 'Pet Sweep', './imgs/newpet-sweep.jpg');
+    var scissors = new catalogItem('scissors', 'Scissors', './imgs/newscissors.jpg');
+    var shark = new catalogItem('shark', 'Shark', './imgs/newshark.jpg');
+    var sweep = new catalogItem('sweep', 'Sweep', './imgs/newsweep.png');
+    var tauntaun = new catalogItem('tauntaun', 'Tauntaun', './imgs/newtauntaun.jpg');
+    var unicorn = new catalogItem('unicorn', 'Unicorn', './imgs/newunicorn.jpg');
+    var usb = new catalogItem('usb', 'USB', './imgs/newusb.gif');
+    var watercan = new catalogItem('watercan', 'Water Can', './imgs/newwater-can.jpg');
+    var wineglass = new catalogItem('wineglass', 'Wine Glass', './imgs/newwine-glass.jpg');
 }
 
 var doAllTheWork = {
@@ -47,106 +48,102 @@ var doAllTheWork = {
         this.nowGroup = [];
     },
     checkLastGroup: function () {
-        return (this.lastGroup.indexOf( this.randoNo ) === -1 );
+        return (this.lastGroup.indexOf(this.randoNo) === -1);
     },
     checkNowGroup: function () {
-        return (this.nowGroup.indexOf( this.randoNo ) === -1);
+        return (this.nowGroup.indexOf(this.randoNo) === -1);
     },
     fillRandomIndicies: function () {
-        while ( this.nowGroup.length < 3) {
+        while (this.nowGroup.length < 3) {
             this.getRandomIndicies();
-            if ( (this.nowGroup.length === 0) && (this.checkLastGroup() === true) ) {
-                this.nowGroup.push( this.randoNo );
+            if ((this.nowGroup.length === 0) && (this.checkLastGroup() === true)) {
+                this.nowGroup.push(this.randoNo);
                 this.randoNo = 0;
-            } else if ( (this.checkLastGroup() === true) && (this.checkNowGroup() === true) ) {
-                this.nowGroup.push( this.randoNo );
+            } else if ((this.checkLastGroup() === true) && (this.checkNowGroup() === true)) {
+                this.nowGroup.push(this.randoNo);
                 this.randoNo = 0;
-            } 
+            }
         }
     },
     populateOptions: function () {
-        for ( var i = 0; i < this.nowGroup.length; i++ ) {
+        for (var i = 0; i < this.nowGroup.length; i++) {
             var imageLoc = document.getElementsByClassName('optionImage')[i];  /*grabs an image tag with the class */
             var wordLoc = document.getElementsByClassName('optionName')[i];  /*grabs a fig caption tag by class */
             var imgSrc = busMallItems[this.nowGroup[i]].itemFilepath; /* grabs an image filepath referenced by index */
             var nameLoc = busMallItems[this.nowGroup[i]].itemName; /* grabs an item name referenced by index */
             var imgId = busMallItems[this.nowGroup[i]].itemId;
             imageLoc.id = imgId;
-            imageLoc.src=imgSrc; /*appends an image filepath to the image src */
+            imageLoc.src = imgSrc; /*appends an image filepath to the image src */
             wordLoc.textContent = nameLoc; /* adds the image name to the fig caption */
             var optionIndex = this.nowGroup[i];
             busMallItems[optionIndex].noTimesShown = busMallItems[optionIndex].noTimesShown + 1; /* adds one to the times shown property of the item manipulated by this loop */
-            }
-            this.trackThisGroup();
-            this.resetThisGroup();
+        }
+        this.trackThisGroup();
+        this.resetThisGroup();
     },
     trackVotes: function () {
         var gotClicked = event.target.id;
-        this.lastGroup.forEach(function(element, index) {
-            if (busMallItems[doAllTheWork.lastGroup[index]].itemId === gotClicked){
+        this.lastGroup.forEach(function (element, index) {
+            if (busMallItems[doAllTheWork.lastGroup[index]].itemId === gotClicked) {
                 busMallItems[doAllTheWork.lastGroup[index]].noTimesClicked = busMallItems[doAllTheWork.lastGroup[index]].noTimesClicked + 1;
             }
         });
     }
 };
 
-var selectOption = document.getElementById( 'eventSite' );
-selectOption.addEventListener( 'click', voteHandler );
+var selectOption = document.getElementById('eventSite');
+selectOption.addEventListener('click', voteHandler);
 var votes = 0;
 
-function voteHandler () {
+function voteHandler() {
     event.preventDefault();
     if (votes > 24) {
         var optionNames = [];
-        var noTimesShownArray = [];
-        var noTimesClickedArray = [];
-        busMallItems.forEach(function(item){
-            optionNames.push(item.itemName);
-            console.log(optionNames);
-            noTimesShownArray.push(item.noTimesShown);
-            console.log(noTimesShownArray);
-            noTimesClickedArray.push(item.noTimesClicked);
-            console.log(noTimesClickedArray);
-        });
-        var context = document.getElementById( 'busMallChart' );
+        // var noTimesShownArray = [];
+        var timesClickedArray = [];
+        if (clicks.length) {
+            for (var i = 0; i < busMallItems.length; i++) {
+                optionNames.push(busMallItems[i].itemName);
+                timesClickedArray.push(busMallItems[i].noTimesClicked + clicks[i]);
+            }
+        } else {
+            for (var j = 0; j < busMallItems.length; j++) {
+                optionNames.push(busMallItems[j].itemName);
+                timesClickedArray.push(busMallItems[j].noTimesClicked);
+            }
+        }
+        var context = document.getElementById('busMallChart');
         var busMallChart = new Chart(context, {
             type: 'bar',
             data: {
                 labels: optionNames,
                 datasets: [{
-                    label: '# of times seen',
-                    data: noTimesShownArray,
-                    backgroundColor: [],
-                    borderColor: [],
-                    borderWidth: 1
-                },
-                {
-                    label: '# of times clicked',
-                    data: noTimesClickedArray,
-                    backgroundColor: [],
-                    borderColor: [],
-                    borderWidth: 1
-                }]
+                        label: '# of times clicked',
+                        data: timesClickedArray,
+                        backgroundColor: [],
+                        borderColor: [],
+                        borderWidth: 1
+                    }]
             },
-            options: { 
+            options: {
                 responsive: true,
                 scales: {
                     yAxes: [{
                         ticks: {
-                            beginAtZero:true
+                            beginAtZero: true
                         }
                     }]
                 }
             }
         });
-
+        localStorage.setItem('storedClicks', JSON.stringify(timesClickedArray));
         return;
     }
-    if ( event.target.src ) {
-    doAllTheWork.trackVotes();
-    doAllTheWork.fillRandomIndicies();
-    doAllTheWork.populateOptions();
-    votes = votes + 1;
+    if (event.target.src) {
+        doAllTheWork.trackVotes();
+        doAllTheWork.fillRandomIndicies();
+        doAllTheWork.populateOptions();
+        votes = votes + 1;
     }
 }
 
